c3db68473d28e41b3a54a1783208b5b1218c9ba8
Jake Swanson <jakeswan213@gmail.com>
2023-12-10 12:57:43 -0600

    Setting up for part 2

---
+++ day1/day1/Inventory.cpp
@@ -0,0 +1,70 @@
+//Jake Swanson 1.2: Inventory implementation
+
+#include "Inventory.h"
+
+Inventory::Inventory() {
+	data = new string[CAPACITY];
+	totalAnswer = 0;
+	index = 0;
+}
+
+bool Inventory::getData(string _fileName_) {
+
+	fstream inFile;
+	inFile.open(_fileName_);
+
+	if (inFile.fail()) return false;
+
+	for (int i = 0; i < CAPACITY; ++i) 
+	{
+		getline(inFile, data[i]);
+	}
+
+	return true;
+}
+
+void Inventory::getTotal() {
+	for (int i = 0; i < CAPACITY; ++i) {
+		string currLine = data[i];
+
+		totalAnswer += (10 * getFirstNum(currLine) + getLastNum(currLine));
+	}
+}
+
+int Inventory::getFirstNum(string _line_) {
+	int firstNum;
+	char tmpChar;
+	
+	for (int i = 0; i < _line_.size(); ++i) {
+		tmpChar = _line_[i];
+
+		if (tmpChar >= '0' && tmpChar <= '9') {
+			firstNum = tmpChar - '0';
+			index = i;
+			return firstNum;
+		}
+	}
+
+	return -1;
+}
+
+int Inventory::getLastNum(string _line_) {
+	int firstNum = -1;
+	char tmpChar;
+
+	for (int i = index; i < _line_.size(); ++i) {
+		tmpChar = _line_[i];
+
+		if (tmpChar >= '0' && tmpChar <= '9') {
+			firstNum = tmpChar - '0';
+		}
+	}
+
+	return firstNum;
+}
+
+void Inventory::printData() {
+	for (int i = 0; i < CAPACITY; ++i) {
+		cout << data[i] << endl;
+	}
+}
---
+++ day1/day1/runner.cpp
@@ -0,0 +1,31 @@
+//Jake Swanson 1.2: runner file
+/*
+* Part 1
+* Given a puzzle input of lines of text,
+* there are numbers in each line.
+* Find the first and last digit to form a 2 digit number.
+* Part 2
+* The numbers include words of numbers (e.i. "one", "two", ...)
+* Combine all the numbers together to get the solution to the puzzle.
+*/
+
+#include "Inventory.h"
+
+int main() {
+
+	Inventory inv;
+	string fileName = "input.txt";
+
+	try {
+		if (!inv.getData(fileName)) throw exception("Could not open file");
+	}
+	catch (exception e) {
+		cerr << e.what() << endl;
+	}
+
+	inv.getTotal();
+
+	cout << inv.viewTotal(); //Answer printed: 54331
+
+	return 0;
+}

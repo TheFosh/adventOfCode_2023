f1cc8c950edd7c2124785cf8ad75dcbc718bdbc8
Jake Swanson <jakeswan213@gmail.com>
2024-01-05 18:17:10 -0600

    Finishing touches
    Mainly just comments for some files

--- day3/day3/Runner.cpp
+++ day3/day3/Runner.cpp
@@ -1,10 +1,14 @@
+//Jake Swanson: Advent of Code 2023 - Day 3
+// Main runner file
+
 #include "Board.h"
 
 int main() {
 	Board myBoard("input.txt");
-
-//	myBoard.printBoard();
-
+	
+	// FOR PART 1 //
 //	cout << myBoard.getTotal() << endl;// My answer- 539590
+
+	// FOR PART 2 //
 //	cout << myBoard.getGearTotal() << endl; // My answer- 80703636
 }
